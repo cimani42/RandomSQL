042a4e3fbbd7fc422252b6c1ffcb00c834e94c73
Papillon42 <98608604+cimani42@users.noreply.github.com>
2024-02-16 15:14:23 +0000

    Update SQL_Practice_Alex_Analyst_joins.sql

--- SQL_Practice_Alex_Analyst_joins.sql
+++ SQL_Practice_Alex_Analyst_joins.sql
@@ -1,37 +1,55 @@
 /*
-WHERE statement: 
-	limits the amount of data pulled
-	specifies the date that you want to return
+INNER JOINS, FULL/LEFT/RIGHT AND OUTER JOINS
 
-=, <>, <, >, And, Or, Like, Null, Not Null, In 
-
+Using a common column between tables. 
 */
 
---SELECT *
---FROM EmployeeDemographics
---WHERE FirstName <> 'Jim' 
+-- INNER JOIN
 
---SELECT *
---FROM EmployeeDemographics
---WHERE FirstName =  'Jim' 
+SELECT *
+FROM SQL_AlexTheAnalyst_Tutorial.dbo.EmployeeDemographics
+	INNER JOIN SQL_AlexTheAnalyst_Tutorial.dbo.EmployeeSalary
+		ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+		-- THIS WILL SHOW EVERYTHING THAT OVERLAPS FROM BOTH TABLES
+		-- MISSING INFROMATION WHERE THERE ARE NOT COMMON FIELDS IN THE RELATED COLUMN
 
---SELECT * 
---FROM EmployeeDemographics
---WHERE Age > 31
+SELECT *
+FROM SQL_AlexTheAnalyst_Tutorial.dbo.EmployeeDemographics
+	OUTER JOIN SQL_AlexTheAnalyst_Tutorial.dbo.EmployeeSalary
+		ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+		-- RETURNS ALL ATTRIBUTES PRESENT IN THE TABLE. 
+		-- SHOULD RETURN 'NULL' WHERE FIELD IS NOT POPULATED
 
---SELECT *
---FROM EmployeeDemographics
---WHERE Age <= 30 AND Gender = 'Male'
+SELECT EmployeeDemographics.EmployeeID, FirstName, LastName, JobTitle, Salary
+FROM SQL_AlexTheAnalyst_Tutorial.dbo.EmployeeDemographics
+	LEFT JOIN SQL_AlexTheAnalyst_Tutorial.dbo.EmployeeSalary
+		ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+		-- LEFT REFERS TO THE FIRST TABLE PRESENT IN THE QUERY 
+		-- THIS INSTACE BEING EMPLOYEE DEMOGRAPHICS
+		-- QUERY RETURNS EVERYTHING PRESENT IN THE FIRST TABLE, AND ALL RELATED IN SECOND TABLE
 
---SELECT *
---FROM EmployeeDemographics
---WHERE LastName Like '%S%' -- Using a wildcard to return data.
+SELECT EmployeeDemographics.EmployeeID, FirstName, LastName, JobTitle, Salary
+FROM SQL_AlexTheAnalyst_Tutorial.dbo.EmployeeDemographics
+	RIGHT JOIN SQL_AlexTheAnalyst_Tutorial.dbo.EmployeeSalary
+		ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+		-- RIGHT REFERS TO THE SECOND TABLE PRESENT IN THE QUERY 
+		-- THIS INSTANCE IS THE EMPLYEE SALARY
+		-- RETURNS QUERY FOR EVERYTHING IN THE SECOND TABLE, AND RELATED TO FIELDS IN FIRST TABLE
 
---SELECT *
---FROM EmployeeDemographics
---WHERE FirstName IS NOT NULL
+-- THE LEFT AND RIGHT JOINS RETURN A TABLE DEPENDENT ON THE INFORMATION PRESENT WITHIN THE COLUMN FOR EMPLOYEE ID
+-- SHOULD ONE TABLE HAVE MORE CRITERIA IN THE CHOSEN COLUMN, THE QUERY WILL WILL BE DIFFERENT BETWEEN THE TWO. 
+-- FOR EXAMPLE, EMPLOYEE SALARY EMPLOYEE ID HAS 6 FIELDS WHILE EMPLOYEE DEMOGRAPHICS EMPLOYEE ID HAS 30.... 
 
---SELECT *
---FROM EmployeeDemographics
---WHERE FirstName IN ('Jim', 'Michael', 'Toby')
----- 'IN' is similar to having multiple equal statments (=)
+SELECT EmployeeDemographics.EmployeeID, FirstName, LastName, Salary
+FROM SQL_AlexTheAnalyst_Tutorial.dbo.EmployeeDemographics
+	INNER JOIN SQL_AlexTheAnalyst_Tutorial.dbo.EmployeeSalary
+		ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+WHERE FirstName <> 'Michael'
+ORDER BY Salary DESC 
+
+SELECT JobTitle, AVG(Salary)
+FROM SQL_AlexTheAnalyst_Tutorial.dbo.EmployeeDemographics
+	INNER JOIN SQL_AlexTheAnalyst_Tutorial.dbo.EmployeeSalary
+		ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+WHERE JobTitle = 'Salesman'
+GROUP BY JobTitle
